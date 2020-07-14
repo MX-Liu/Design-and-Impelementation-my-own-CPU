9f40c80a7ebcf944c7870d4808cb197849c63fab
MX-Liu <45260284+MX-Liu@users.noreply.github.com>
2020-07-14 09:11:29 +0200

    update image file

---
+++ rtl - 5/mips_application_image.vhdl
@@ -0,0 +1,31 @@
+-- The generate imaginge script come from the NEO430 Processor Project, by Stephan Nolting,
+-- Neo430 is a 16-bit processor, this mips cpu project is 32 bit processor
+-- Auto-generated memory init file (for APPLICATION)
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+package mips_application_image is
+
+  type application_init_image_t is array (0 to 65535) of std_ulogic_vector(31 downto 0);
+  constant application_init_image : application_init_image_t := (
+    00000000 => x"3c020404",
+    00000001 => x"34420404",
+    00000002 => x"34070007",
+    00000003 => x"34050005",
+    00000004 => x"34080008",
+    00000005 => x"0000000f",
+    00000006 => x"00021200",
+    00000007 => x"00e21004",
+    00000008 => x"00021202",
+    00000009 => x"00a21006",
+    00000010 => x"00000000",
+    00000011 => x"000214c0",
+    00000012 => x"00000040",
+    00000013 => x"00021403",
+    00000014 => x"01021007",
+    others => x"00000000"
+    
+  );
+
+end mips_application_image;
---
+++ rtl - 6/mips_application_image.vhdl
@@ -0,0 +1,32 @@
+-- The generate imaginge script come from the NEO430 Processor Project, by Stephan Nolting,
+-- Neo430 is a 16-bit processor, this mips cpu project is 32 bit processor
+-- Auto-generated memory init file (for APPLICATION)
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+package mips_application_image is
+
+  type application_init_image_t is array (0 to 65535) of std_ulogic_vector(31 downto 0);
+  constant application_init_image : application_init_image_t := (
+    00000000 => x"3c010000",
+    00000001 => x"3c02ffff",
+    00000002 => x"3c030505",
+    00000003 => x"3c040000",
+    00000004 => x"0041200a",
+    00000005 => x"0061200b",
+    00000006 => x"0062200b",
+    00000007 => x"0043200a",
+    00000008 => x"00000011",
+    00000009 => x"00400011",
+    00000010 => x"00600011",
+    00000011 => x"00002010",
+    00000012 => x"00600013",
+    00000013 => x"00400013",
+    00000014 => x"00200013",
+    00000015 => x"00002012",
+    others => x"00000000"
+    
+  );
+
+end mips_application_image;
